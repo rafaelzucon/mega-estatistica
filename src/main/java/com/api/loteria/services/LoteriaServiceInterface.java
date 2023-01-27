66e9b1004d7770ee39ccf7d3bc65bb6208862e0d
Rafael Zucon <rafaelzucon@yahoo.com.br>
2023-01-27 12:41:47 -0300

    use case

--- src/main/java/com/api/loteria/services/LoteriaServiceInterface.java
+++ src/main/java/com/api/loteria/services/LoteriaServiceInterface.java
@@ -5,6 +5,7 @@ import com.api.loteria.models.Nro1LoteriaModelCount;
 
 import java.util.List;
 
+// NATIVE QUERY NO SPRING BOOT REQUER INTERFACE ENTRE O MODELO E A APRESENTAÇÃO
 public interface LoteriaServiceInterface {
     List<Nro1LoteriaModelCount> getReportByNativeQuery();
     List<LoteriaModel> findAll();
