b0ba9a2e96e5831718984a179f2f2b6bb2a4ad3e
Rafael Zucon <rafaelzucon@yahoo.com.br>
2023-01-27 12:36:09 -0300

    config

--- src/main/java/com/api/loteria/models/Nro1LoteriaModelCountInterface.java
+++ src/main/java/com/api/loteria/models/Nro1LoteriaModelCountInterface.java
@@ -2,6 +2,7 @@ package com.api.loteria.models;
 
 import java.math.BigDecimal;
 
+// NATIVE QUERY NO SPRING BOOT REQUER INTERFACE ENTRE O MODELO E A APRESENTAÇÃO
 public interface Nro1LoteriaModelCountInterface {
     String getCountNro1();
     String getCont1();
